a28fb43c95f0c59e6cf0e00e245a5d078f64373e
smarttechfire <educationfire552@gmail.com>
2024-06-05 23:28:04 +0530

    header & nav

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,30 @@
+import Link from "next/link";
+import Nav from "./Nav";
+import { Button } from "./ui/button";
 
 const Header = () => {
   return (
-    <header>
-      Header
+    <header className=" py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between">
+        <Link href="/">
+          <h1 className=" text-4xl font-semibold">
+            SMart <span className=" text-accent">.</span>
+          </h1>
+        </Link>
+        {/* desktop nav */}
+        <div className=" hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>
+                Hire me
+            </Button>
+          </Link>
+        </div>
+        {/* mobile nav */}
+        <div className=" xl:hidden">Mobile Nav</div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
