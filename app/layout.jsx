f4bc9bc3e314063f897b752a486dbafb46539cbc
smarttechfire <educationfire552@gmail.com>
2024-06-07 19:03:35 +0530

    complete the pageTransition

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,12 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
 
-const jetBrains_Mono = JetBrains_Mono({ 
+const jetBrains_Mono = JetBrains_Mono({
   subsets: ["latin"],
-  weight: ["100","200","300","400","500","600","700","800"],
-  variable: '--font-jetbrainsMono'
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-jetbrainsMono",
 });
 
 export const metadata = {
@@ -17,8 +18,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={jetBrains_Mono.variable}>
-        <Header/>
-        {children}
+        <Header />
+        <PageTransition>{children}</PageTransition>
       </body>
     </html>
   );
---
+++ app/work/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div>
+      Work page
+    </div>
+  )
+}
+
+export default Work
